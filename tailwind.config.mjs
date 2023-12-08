835b0a611a34852c097ce266de31474f1b9d8779
Nathan Lawson <nathan@cargocreative.co.uk>
2023-12-08 15:28:20 +0000

    Add Bricolage Grotesque font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'display': ['Bricolage Grotesque', 'system-ui', 'sans-serif'],
+			'body': ['"Open Sans"', 'system-ui', 'sans-serif'],
+		},
 	},
 	plugins: [],
 }
